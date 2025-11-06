8ec031d657f1f7bb415b318e00036a792ca468bb
MatildaSU24B <matildaljunggren08@gmail.com>
2025-11-06 09:50:36 +0100

    uppgrades

--- ReigningCats&Dogs/Assets/HerbertScripts/treasury.cs
+++ ReigningCats&Dogs/Assets/HerbertScripts/treasury.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class ReturnLoot : MonoBehaviour
 {
-    public int lootingEffiency;
+    public float lootingEffiency;
     [SerializeField] int netWorth = 0;
     [SerializeField] TextMeshProUGUI treasuryText;
-    
+
     [SerializeField] int Upgrade1cost;
-
-   
-   
     [SerializeField] int Upgrade2cost;
-    [SerializeField] bool Upgrade2purchased;
-
+    
     [SerializeField] float TimeBetweenSpawns;
     void Start()
     {
         lootingEffiency = 1;
         treasuryText.text = "You have 0 Gold";
-       
+        Upgrade1cost = 1000;
+        Upgrade2cost = 500;
+        TimeBetweenSpawns = 5.5f;
     }
 
     // Update is called once per frame
@@ -54,23 +52,23 @@ public class ReturnLoot : MonoBehaviour
     void LootGet()
     {
         int lootValue = Random.Range(1, 3);
-        lootValue = lootValue * 10 * lootingEffiency;
+        lootValue = (int)(lootValue * 10 * lootingEffiency);
         netWorth += lootValue;
         treasuryText.text = "You have " + netWorth.ToString() + " Gold";
         Debug.Log("My liege, i have returned with" + netWorth.ToString() + "Gold!");
     }
 
 
-    public void Upgrade1()
+    public void Upgrade2()
     {    
-         if (netWorth >= Upgrade1cost)
+         if (netWorth >= Upgrade2cost)
          {
                 if (TimeBetweenSpawns >= 1)
                 {
-                netWorth = netWorth - Upgrade1cost;
+                netWorth = netWorth - Upgrade2cost;
                 treasuryText.text = "You have " + netWorth.ToString() + " Gold";
-                StartCoroutine(Upgrade1cor());
-                Upgrade1cost = Upgrade1cost * 5;    
+                StartCoroutine(Upgrade2cor());
+                Upgrade2cost = Upgrade2cost * 5;    
                 TimeBetweenSpawns = TimeBetweenSpawns - 0.5f;
                 }
 
@@ -83,30 +81,31 @@ public class ReturnLoot : MonoBehaviour
          
     
 
-    IEnumerator Upgrade1cor()
+    IEnumerator Upgrade2cor()
     {
         yield return new WaitForSeconds(TimeBetweenSpawns);
         sendSoldier(0);
-        StartCoroutine(Upgrade1cor());
+        StartCoroutine(Upgrade2cor());
     }
-        void Upgrade2()
+        public void Upgrade1()
     {
-        
-        if (Upgrade2purchased == false)
-        {
             if (netWorth >= Upgrade1cost)
             {
 
-                netWorth -= Upgrade2cost;
-                Upgrade2purchased = true;
-                lootingEffiency += 1;
+                netWorth -= Upgrade1cost;
+                if (lootingEffiency == 1)
+                {
+                    lootingEffiency += 0.2f;
+                Upgrade1cost = Upgrade1cost * 5;
+                }
+                
             }
             else
             {
                 Debug.Log("Not enough gold!");
             }
 
-        }
+        
         
     }
 }
